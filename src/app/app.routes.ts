a3cb9b16f6896b3a46a5feed69fcd8d2c248a13d
Bruno Donatelli <bruno.donatelli17@gmail.com>
2026-03-25 18:40:32 -0300

    feat: add login routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  {
+    path: 'login', children: [
+      { path: 'reactive', loadComponent: () => import('./reactive/login/login').then(c => c.Login) },
+      { path: 'signal', loadComponent: () => import('./signal/login/login').then(c => c.Login) },
+    ]
+  },
+];
